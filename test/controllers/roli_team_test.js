d7a44b05e438383b8846d5caff5530d6bac1e4a8
Louis Pilfold <louis@lpil.uk>
2015-11-27 18:10:20 +0000

    Remove extra describe block

--- test/controllers/roli_team_test.js
+++ test/controllers/roli_team_test.js
@@ -2,24 +2,22 @@
 
 import { gatherTeam } from 'controllers/roli_team';
 
-describe('tests', () => {
-  describe('gatherTeam', () => {
-    it('queries a given document for team members', () => {
+describe('gatherTeam', () => {
+  it('queries a given document for team members', () => {
 
-      const fakeNodes = [
-        { children: [ { src: 'foo.png' }, { textContent: 'Matt Damon' } ] },
-        { children: [ { src: 'bar.png' }, { textContent: 'Grace Hopper' } ] },
-      ];
-      var qsaStub = function(selector) {
-        assert.equal(selector, '.team-member');
-        return fakeNodes;
-      };
-      const result = gatherTeam({ querySelectorAll: qsaStub });
-      const expected = [
-        { image: 'bar.png', name: 'Grace Hopper' },
-        { image: 'foo.png', name: 'Matt Damon' },
-      ];
-      assert.deepEqual(result, expected);
-    });
+    const fakeNodes = [
+      { children: [ { src: 'foo.png' }, { textContent: 'Matt Damon' } ] },
+      { children: [ { src: 'bar.png' }, { textContent: 'Grace Hopper' } ] },
+    ];
+    var qsaStub = function(selector) {
+      assert.equal(selector, '.team-member');
+      return fakeNodes;
+    };
+    const result = gatherTeam({ querySelectorAll: qsaStub });
+    const expected = [
+      { image: 'bar.png', name: 'Grace Hopper' },
+      { image: 'foo.png', name: 'Matt Damon' },
+    ];
+    assert.deepEqual(result, expected);
   });
 });
